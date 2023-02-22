db743536f7f70a7f1859922804b9e99608826513
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2023-02-22 10:09:24 +0100

    Added nice custom colours

---
+++ TimeTracker/TimeTracker/Common/Extensions/SwiftUI/Asset+Utils.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+extension SwiftUI.Color {
+  init(_ asset: ColorAsset) {
+    self.init(asset.name)
+  }
+}
+extension Text {
+  func foregroundColor(_ colorAsset: ColorAsset) -> Text {
+    foregroundColor(SwiftUI.Color(colorAsset))
+  }
+}
+
+extension Shape {
+  func stroke(_ colorAsset: ColorAsset, lineWidth: CGFloat = 1) -> some View {
+    stroke(SwiftUI.Color(colorAsset), lineWidth: lineWidth)
+  }
+}
+
+extension View {
+  func background(_ colorAsset: ColorAsset, alignment: Alignment = .center) -> some View {
+    background(SwiftUI.Color(colorAsset), alignment: alignment)
+  }
+
+  func foregroundColor(_ colorAsset: ColorAsset) -> some View {
+    foregroundColor(SwiftUI.Color(colorAsset))
+  }
+}

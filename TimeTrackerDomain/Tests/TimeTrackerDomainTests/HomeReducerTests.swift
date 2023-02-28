75f0f7a9abf0a628272f57557f1e8dc1381395ab
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2023-02-28 11:06:23 +0100

    Improvements to HomeReducer tests
    * Used a TestStore creation closure to specify dependencies.

--- TimeTrackerDomain/Tests/TimeTrackerDomainTests/HomeReducerTests.swift
+++ TimeTrackerDomain/Tests/TimeTrackerDomainTests/HomeReducerTests.swift
@@ -7,16 +7,21 @@ import TimeTrackerModel
 @MainActor
 final class HomeReducerTests: XCTestCase {
   func testEntriesLoadedOnFirstAppear() async {
-    let reducer = HomeReducer()
-    let store = TestStore(initialState: HomeReducer.State(), reducer: reducer)
     let entries: [Entry] =  [.mock, .mock]
-    store.dependencies.entriesRepository = TestEntriesRepository(mockedEntries: entries)
+    let reducer = HomeReducer()
+    let store = TestStore(
+      initialState: HomeReducer.State(),
+      reducer: reducer
+    ) {
+      $0.entriesRepository = TestEntriesRepository(mockedEntries: entries)
+    }
 
-    await store.send(.onFirstAppear)
+    let task = await store.send(.onFirstAppear)
     await store.receive(.onEntriesLoaded(entries)) {
       $0.entries = entries.sorted(by: { $0.start > $1.start })
     }
-    await store.skipInFlightEffects()
+
+    await task.cancel()
   }
 
   func testEntryDeletion() async {
